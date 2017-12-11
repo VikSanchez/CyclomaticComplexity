a8ba0ad52921fe166ac0a75526a3a9b8d4b969f9
VikasDanny <vikasdanny56@gmail.com>
2017-12-11 13:17:57 +0000

    Update master.py

--- master.py
+++ master.py
@@ -17,4 +17,14 @@ def main():
     git_url = "https://github.com/VikasDanny/CyclomaticComplexity.git"
     repo_dir = "/home/Vikas/Cyclomatic-Complexity/test"
     directory = "/home/Vikas/Cyclomatic-Complexity/results/"
+    async_results = {}
+    connection = sqlite3.connect('results.db')
+    connection.execute('''CREATE TABLE RESULTS
+                                             (FileName TEXT PRIMARY KEY     NOT NULL,
+                                              CC            REAL,
+                                             TimeTaken      REAL    NOT NULL);''')
+    connection.execute('''CREATE TABLE WORKERS
+                                                 (WORKER INT PRIMARY KEY     NOT NULL,
+                                                 TimeTaken      REAL    NOT NULL);''')
+    print "Table Created."
     

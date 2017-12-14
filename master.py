c86f06ea4e99bc00996e0b9b953042f8762c7049
VikasDanny <vikasdanny56@gmail.com>
2017-12-14 14:30:21 +0000

    Update master.py

--- master.py
+++ master.py
@@ -70,6 +70,25 @@ def main():
                                 (worker, total_time))
     connection.commit()
     print('Done')
+        x = []
+    y = []
+    cursor1 = connection.execute("SELECT WORKER, TimeTaken FROM WORKERS")
+    for row in cursor1.fetchall():
+        x.append(row[0])
+        y.append(row[1])
+        #print x
+        #print y
+
+    print x
+    print y
+
+    #y_pos = np.arange(len(x))
+    plt.plot(x, y, 'r')
+    plt.xlabel('Number of Workers')
+    plt.ylabel('Time')
+    plt.title('Performance of Workers')
+    plt.bar(x, y)
+    plt.show()
     
     if __name__ == '__main__':
     with Connection():
